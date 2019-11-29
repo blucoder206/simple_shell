4d312d321eb3ca166abe62b423dcf53c513cd7aa
cmlesquivel <anayacamilo@yahoo.es>
2019-11-29 20:53:07 +0000

    split the main

--- holberton.h
+++ holberton.h
@@ -16,4 +16,10 @@ int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *_getenv(char *name);
 void ctrlc_handle(int sig);
+char *_getenv(char *pathaddress);
+char **input_split(char **argv, char *string, char *delimitator);
+void processing(ssize_t readed_bytes, char **argv, char *string);
+void print_my_command(char *pathname, char *argv[], int is_heap);
+void brain(char **argv, struct stat st, char **paths);
+
 #endif
