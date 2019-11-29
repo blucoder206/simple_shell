c1185ef079bfa9186b1afb507a5c04268dd98e1a
cmlesquivel <anayacamilo@yahoo.es>
2019-11-29 19:30:45 +0000

    fix betty

--- main.c
+++ main.c
@@ -5,24 +5,24 @@
 *
 * Return: value of variable PATH
 */
-char *_getenv (char *pathaddress)
+char *_getenv(char *pathaddress)
 {
-  char *cpy_nm = NULL;
-  int i, len_value;
+	char *cpy_nm = NULL;
+	int i, len_value;
 
-  cpy_nm = malloc ((_strlen (pathaddress) + 2) * (sizeof (char)));
-  _strcpy (cpy_nm, pathaddress);
-  _strcat (cpy_nm, "=");
-  len_value = _strlen (cpy_nm);
-  for (i = 0; environ[i] != NULL; i++)
-    {
-      if (strncmp (cpy_nm, environ[i], len_value) == 0)
+	cpy_nm = malloc((_strlen(pathaddress) + 2) * (sizeof(char)));
+	_strcpy(cpy_nm, pathaddress);
+	_strcat(cpy_nm, "=");
+	len_value = _strlen(cpy_nm);
+	for (i = 0; environ[i] != NULL; i++)
 	{
-	  free (cpy_nm);
-	  return (environ[i] + len_value);
+		if (strncmp(cpy_nm, environ[i], len_value) == 0)
+		{
+			free(cpy_nm);
+			return (environ[i] + len_value);
+		}
 	}
-    }
-  return (NULL);
+	return (NULL);
 }
 
 /**
@@ -33,22 +33,20 @@ char *_getenv (char *pathaddress)
 *
 * Return: argv
 **/
-char **input_split (char **argv, char *string, char *delimitator)
-{
-  char *token;
-  int i = 0;
 
-  token = strtok (string, delimitator);
-  while (token)
-    { 
-      argv[i++] = token;
-      token = strtok (NULL, delimitator);
-    }
-    
-  argv[i] = 0;
-  
-  return (argv);
-  
+char **input_split(char **argv, char *string, char *delimitator)
+{
+	char *token;
+	int i = 0;
+
+	token = strtok(string, delimitator);
+	while (token)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, delimitator);
+	}
+	argv[i] = 0;
+	return (argv);
 }
 
 /**
@@ -59,54 +57,54 @@ char **input_split (char **argv, char *string, char *delimitator)
 *
 * Return: nothing
 **/
-void processing (ssize_t readed_bytes, char **argv, char *string)
-{ 
-  if (readed_bytes == -1)
-    {
-     
-       if (string != NULL)
-	{
-        free (string);
-	  
-	}
-     if (argv != NULL)
-	{
-        free (argv);
-	}
-      printf ("\n");
-      exit (1);
-    }
-  else
-    {
-      input_split (argv, string, " \n");
-      if ((argv[0]) == NULL)
-	{
-	}
-      else if (strcmp (argv[0], "env") == 0)
-	{
-	  int env_i = 0;
 
-	  while (environ[env_i] != NULL)
-	    {
-	      printf ("%s\n", environ[env_i]);
-	      env_i++;
-	    }
-	}
-      else if (strcmp (argv[0], "exit") == 0)
+void processing(ssize_t readed_bytes, char **argv, char *string)
+{
+	if (readed_bytes == -1)
 	{
-	  if (argv != NULL)
-	    {
-          free (string);
-	      free (argv);
-	    }
-	  exit (0);
+		if (string != NULL)
+		{
+			free(string);
+		}
+		if (argv != NULL)
+		{
+			free(argv);
+		}
 	}
-    }
+	printf("\n");
+	exit(1);
+}
+else
+{
+	input_split(argv, string, " \n");
+	if ((argv[0]) == NULL)
+	{
+	}
+	else if (strcmp(argv[0], "env") == 0)
+	{
+		int env_i = 0;
+
+		while (environ[env_i] != NULL)
+		{
+			printf("%s\n", environ[env_i]);
+			env_i++;
+		}
+	}
+	else if (strcmp(argv[0], "exit") == 0)
+	{
+		if (argv != NULL)
+		{
+			free(string);
+			free(argv);
+		}
+		exit(0);
+	}
+}
 }
 
 
 /**
-* print_my_command - run the command execve 
+* print_my_command - run the command execve
 * @argv: user input
 * @is_heap: indicate if pathnam is the heap
 * @paths: paths taken from environ
@@ -116,30 +114,29 @@ void processing (ssize_t readed_bytes, char **argv, char *string)
 
 void print_my_command(char *pathname, char *argv[], int is_heap)
 {
-    int status;
-      pid_t child_pid;
+	int status;
+	pid_t child_pid;
 
-      child_pid = fork ();
-      if (child_pid == 0)
+	child_pid = fork();
+	if (child_pid == 0)
 	{
-	  if (execve (pathname, argv, NULL) == -1)
-      {
-	    printf ("Error\n");
-        if(is_heap == 1)
-        {
-            free (pathname);
-        }
-      }
+		if (execve(pathname, argv, NULL) == -1)
+		{
+			printf("Error\n");
+			if (is_heap == 1)
+			{
+				free(pathname);
+			}
+		}
 	}
-      else
-      {
-	wait (&status);
-      if(is_heap == 1)
-        {
-            free (pathname);
-        }
-      }   
-
+	else
+	{
+		wait(&status);
+		if (is_heap == 1)
+		{
+			free(pathname);
+		}
+	}
 }
 
 /**
@@ -150,48 +147,46 @@ void print_my_command(char *pathname, char *argv[], int is_heap)
 *
 * Return: nothing
 **/
-void brain (char **argv, struct stat st, char **paths)
+void brain(char **argv, struct stat st, char **paths)
 {
-  int i, found = 0;
+	int i, found = 0;
+	char *executable = NULL;
 
-
-  char *executable = NULL;  
-
-  if (stat (argv[0], &st) == 0)
-    {   
-    print_my_command(argv[0], argv, 0);
-    }
-  else
-    {
-      for (i = 0; paths[i] != NULL; i++)
+	if (stat(argv[0], &st) == 0)
 	{
-	  executable =malloc ((_strlen (paths[i]) + _strlen (argv[0]) +2) * sizeof (char));
-	  _strcat (executable, paths[i]);
-	  _strcat (executable, "/");
-	  _strcat (executable, argv[0]);
-
-	  if (stat (executable, &st) == 0)
-	    {
-
-             print_my_command(executable, argv, 1);
-             found = 1;
-             break; 
-	    }
-	  else
-	    {
-	      if (executable != NULL)
-          {
-		free (executable);
-        executable=NULL;
-          }
-	    }
+		print_my_command(argv[0], argv, 0);
 	}
-    if(!found)
-    {
-      printf ("%s: not found\n", argv[0]);
-    }
-    }
+	else
+	{
+		for (i = 0; paths[i] != NULL; i++)
+		{
+			executable = malloc((_strlen(paths[i]) + _strlen(argv[0]) + 2) * sizeof(char));
+			_strcat(executable, paths[i]);
+			_strcat(executable, "/");
+			_strcat(executable, argv[0]);
+
+			if (stat(executable, &st) == 0)
+			{
+				print_my_command(executable, argv, 1);
+				found = 1;
+				break;
+			}
+			else
+			{
+				if (executable != NULL)
+				{
+					free(executable);
+					executable = NULL;
+				}
+			}
+		}
+		if (!found)
+		{
+			printf("%s: not found\n", argv[0]);
+		}
+	}
 }
+
 
 
 /**
@@ -200,35 +195,32 @@ void brain (char **argv, struct stat st, char **paths)
 * Return: nothing
 **/
 
-int main (void)
+int main(void)
 {
-  char *paths[20];
-  char *path;
+	char *paths[20];
+	char *path;
 
-  signal (SIGINT, ctrlc_handle);
-  path = _getenv ("PATH");
-  input_split (paths, path, ":");
-  
-   for (;;)
-    {
-      ssize_t readed_bytes;
-      struct stat st;
-      size_t numberbytes = 0;
-      char *string = NULL;
-      char **argv;     
+	signal(SIGINT, ctrlc_handle);
+	path = _getenv("PATH");
+	input_split(paths, path, ":");
 
-      printf ("$ ");
-      readed_bytes = getline (&string, &numberbytes, stdin);
-     
-      argv = malloc (readed_bytes * sizeof (*argv));      
-       
-      processing (readed_bytes, argv, string);      
+	for (;;)
+	{
+		ssize_t readed_bytes;
+		struct stat st;
+		size_t numberbytes = 0;
+		char *string = NULL;
+		char **argv;
 
-      brain (argv, st, paths);
-     
-      free(string);
-     free(argv);
-     
-    }
-  return (0);
+		printf("$ ");
+		readed_bytes = getline(&string, &numberbytes, stdin);
+		argv = malloc(readed_bytes * sizeof(*argv));
+
+		processing(readed_bytes, argv, string);
+		brain(argv, st, paths);
+
+		free(string);
+		free(argv);
+	}
+	return (0);
 }
